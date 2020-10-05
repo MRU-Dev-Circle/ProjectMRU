6ec0ef54f5a4815800c6e98bb627f375e84024f2
Sachin Gupta <sachin070gupta@gmail.com>
2020-10-05 17:07:08 +0530

    Update LinearSearch.java
    Some minor chnges.

--- Scripts/Java/Searching & Sorting Algos/LinearSearch.java
+++ Scripts/Java/Searching & Sorting Algos/LinearSearch.java
@@ -4,8 +4,11 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {1,4,8,10,23,25,29,30};
         int elt = 29;
-        int ind = linearSearch(arr,elt);
-        System.out.println("Searching element "+elt+" using linear search : "+ind);
+        int ind = linearSearch(arr,elt);  
+        if(ind == -1)
+          System.out.println("Element "+elt+" not present in the array");
+        else
+          System.out.println("Searching element "+elt+" using Linear search : "+ind);
     }
 
     public static int linearSearch(int[] arr, int elt) {
