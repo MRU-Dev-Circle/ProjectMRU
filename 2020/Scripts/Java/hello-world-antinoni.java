232f7a83cca63760c3dda139916f8461f5f54d82
c0sm0s <minhajulhossain@gmail.com>
2021-10-02 19:18:43 +0530

    ready for 2021

---
+++ 2020/Scripts/Java/hello-world-antinoni.java
@@ -0,0 +1,9 @@
+//Language: Java
+//Author: antinoni
+//Github: https://github.com/antinoni
+
+class HelloProjectMRU {
+    public static void main(String[] args) {
+        System.out.println("Hello, Project MRU!"); 
+    }
+}
--- Scripts/Java/hello-world-antinoni.java
+++
@@ -1,9 +0,0 @@
-//Language: Java
-//Author: antinoni
-//Github: https://github.com/antinoni
-
-class HelloProjectMRU {
-    public static void main(String[] args) {
-        System.out.println("Hello, Project MRU!"); 
-    }
-}
